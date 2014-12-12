03bc07de5ec3c7c957cf43c1df9ed66ed3019fbc
William Benton <self@willbenton.com>
2014-12-12 12:04:47 -0600

    initial revision

---
+++ common/src/main/scala/com/redhat/et/consigliere/common/app.scala
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the "consigliere" toolkit for sosreport
+ * data analytics and visualization.
+ *
+ * Copyright (c) 2014 Red Hat, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.c
+ */
+
+package com.redhat.et.consigliere.common;
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+trait Common {
+  private type ConfXf = SparkConf => SparkConf
+  private var exitHooks: List[() => Unit] = Nil
+  private var configHooks: List[ConfXf] = Nil
+    
+  private lazy val _conf = { 
+    val initialConf = new SparkConf()
+     .setMaster(master)
+     .setAppName(appName)
+     .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+
+    configHooks.reverse.foldLeft(initialConf) {(c, f) => f(c)}
+  }
+    
+    private lazy val _context = { 
+      new SparkContext(_conf)
+    }
+    
+    def master = sys.env.getOrElse("C9E_MASTER", "local[8]")
+    def appName = "consigliere"
+    
+    def main(args: Array[String]) = {
+      appMain(args)
+      runExitHooks
+    }
+    
+    def addConfig(xform: SparkConf => SparkConf) {
+      configHooks = xform :: configHooks
+    }
+    
+    def addExitHook(thunk: => Unit) {
+      exitHooks = {() => thunk} :: exitHooks
+    }
+    
+    def runExitHooks() {
+      for (hook <- exitHooks) {
+        hook()
+      }
+    }
+    
+    def appMain(args: Array[String]): Unit
+    
+    def context: SparkContext = _context
+}
---
+++ project/Build.scala
@@ -0,0 +1,75 @@
+import sbt._
+import Keys._
+
+object CBuild  extends Build {
+  val VERSION = "0.0.1-SNAPSHOT"
+  
+  lazy val analysis = project settings(analysisSettings : _*)
+  
+  lazy val common = project settings(commonSettings : _*)
+  
+  def baseSettings = Defaults.defaultSettings ++ Seq(
+    organization := "com.redhat.et",
+    version := VERSION,
+    scalaVersion := "2.10.4",
+    resolvers ++= Seq(
+      "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+      "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/",
+      "Akka Repo" at "http://repo.akka.io/repository",
+      "spray" at "http://repo.spray.io/"
+    ),
+    libraryDependencies ++= Seq(
+        "com.github.nscala-time" %% "nscala-time" % "0.6.0",
+        "io.spray" %%  "spray-json" % "1.2.5",
+        "org.json4s" %%  "json4s-jackson" % "3.2.6"
+    ),
+    scalacOptions ++= Seq("-feature", "-Yrepl-sync", "-target:jvm-1.7")
+  )
+  
+  def sparkSettings = Seq(
+    libraryDependencies ++= Seq(
+        "org.apache.spark" %% "spark-core" % sparkVersion,
+        "org.apache.spark" %% "spark-sql" % sparkVersion,
+        "org.apache.spark" %% "spark-mllib" % sparkVersion
+    )
+  )
+  
+  def breezeSettings = Seq(
+    libraryDependencies ++= Seq(
+      "org.scalanlp" %% "breeze" % "0.6"
+    )
+  )
+  
+  def testSettings = Seq(
+    fork := true,
+    libraryDependencies ++= Seq(
+      "org.scalacheck" %% "scalacheck" % "1.11.3" % "test"
+    )
+  )
+  
+  def dispatchSettings = Seq(
+    libraryDependencies += 
+      "net.databinder.dispatch" %% "dispatch-core" % "0.11.1"
+  )
+  
+  def commonSettings = baseSettings ++ sparkSettings
+  
+  def analysisSettings = baseSettings ++ sparkSettings ++ breezeSettings ++ testSettings ++ Seq(
+    initialCommands in console :=
+      """
+        |import org.apache.spark.SparkConf
+        |import org.apache.spark.SparkContext
+        |import org.apache.spark.rdd.RDD
+        |val conf = new SparkConf().setMaster("local[8]").setAppName("analysis console").set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        |val spark = new SparkContext(conf)
+        |val sqlContext = new org.apache.spark.sql.SQLContext(spark)
+        |import sqlContext._
+        |
+      """.stripMargin,
+    cleanupCommands in console := "spark.stop"
+  )
+  
+  val sparkVersion = "1.1.0"
+  val scalatraVersion = "2.2.2"
+  val scalateVersion = "1.7.0-SNAPSHOT"
+}
