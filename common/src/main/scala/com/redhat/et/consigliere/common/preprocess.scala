d91fd549f1d70201db18db0836cbba2d4d3549b9
William Benton <willb@redhat.com>
2014-12-13 20:59:08 -0600

    added scala implementation of ensureDir

---
+++ common/src/main/scala/com/redhat/et/consigliere/common/preprocess.scala
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the "consigliere" toolkit for sosreport
+ * data analytics and visualization.
+ *
+ * Copyright (c) 2014 Red Hat, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.redhat.et.consigliere.common;
+
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+object SosReportPreprocessor {
+  import java.io.File
+  import scala.util.{Try, Success, Failure}
+  
+  def ensureDir(dirname: String): Try[String] = {
+    val dir = new File(dirname)
+    (dir.exists, dir.isDirectory) match {
+      case (true, true) => Success(dirname)
+      case (true, false) => Failure(new RuntimeException(s"$dirname already exists but is not a directory"))
+      case (false, _) => Try(Pair(dir.mkdirs(), dirname)._2)
+    }
+  }
+
+  
+}
