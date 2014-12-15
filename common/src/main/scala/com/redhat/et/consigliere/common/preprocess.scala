ac5461d4d93b8aea0dd3f07c5959c5c121bd60bc
William Benton <self@willbenton.com>
2014-12-15 13:51:15 -0600

    added some cleaning transformations

--- common/src/main/scala/com/redhat/et/consigliere/common/preprocess.scala
+++ common/src/main/scala/com/redhat/et/consigliere/common/preprocess.scala
@@ -22,7 +22,73 @@ package com.redhat.et.consigliere.common;
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
+trait CleaningHelpers {
+  def splitValue(sls: String, delim: String = " ") = {
+    JArray(sls.split(delim).map(JString(_)).toList)
+  }
+  
+  def splitRpmList(sls: String) = {
+    JArray(
+      sls.split("\n").map {
+        case RpmSplit(pkg, date) => 
+          JObject(List(("rpm", JString(pkg)), ("date", JString(date))))
+        case x => JString(x)
+      }.toList
+    )
+  }
+  
+  val BogusName = "(.*[\\W].*)".r
+  val dashesToCamel = "(-+([a-z]))".r
+  val badChars = "(\\W)".r
+  val RpmSplit = "([^\\s]+)\\s+([^\\s].*)".r
+  
+  def removeBadChars(name: String) = {
+    badChars.replaceAllIn(dashesToCamel.replaceAllIn(name, m => s"${(m group 2).toUpperCase}"), "")
+  }
+}
+
+trait CleaningTransformations extends CleaningHelpers {
+  type FieldX = PartialFunction[JField, JField]
+  type ValueX = PartialFunction[JValue, JValue]
+  
+  val sanitizeNames: FieldX = {
+    case JField(BogusName(name), v) => JField(removeBadChars(name), v)
+  }
+  
+  val normalizeBooleans: FieldX = {
+    case JField(name, JString("yes")) => JField(name, JBool(true))
+    case JField(name, JString("no")) => JField(name, JBool(false))
+  }
+  
+  val splitFlags: FieldX = { 
+    case JField("flags", JString(s)) => JField("flags", splitValue(s))
+  }
+  
+  // needs to run after sanitizeNames, obvs
+  val splitRpms: FieldX = {
+    case JField("installedRpms", JString(list)) => {
+      JField("installedRpms", splitRpmList(list))
+    }
+  }
+}
+
+object DefaultTransformations extends CleaningTransformations {
+  val fieldTransforms = List(sanitizeNames,
+    normalizeBooleans,
+    splitFlags,
+    splitRpms
+  )
+  
+  val valueTransforms = List[ValueX]()
+    
+  def apply(o: JValue): JValue = {
+   val withFields = (o /: fieldTransforms)({(o, x) => o.transformField(x)})
+   (withFields /: valueTransforms)({(o, x) => o.transform(x) })
+  }
+}
+
 object SosReportPreprocessor {
+  // TODO:  it would make sense to have custom case classes for these
   import java.io.{File, FileReader, FileWriter}
   import scala.util.{Try, Success, Failure}
   
@@ -43,10 +109,11 @@ object SosReportPreprocessor {
     parsedFile.map(_ match { 
       case JArray(jls) => jls.collect { case j:JObject => j }
       case o: JObject => List(o)
+      case _ => List[JObject]()
     })
   }
-
-  def partitionByKinds(jls: List[JObject]): Map[String, Vector[JObject]] = {
+  
+  def partitionByKinds(jls: List[JObject], xform: JObject => JObject = {x => x}): Map[String, Vector[JObject]] = {
     implicit val formats = new org.json4s.DefaultFormats {}
     
     def partitionOne(m: Map[String, Vector[JObject]], jv: JObject) = {
@@ -54,12 +121,12 @@ object SosReportPreprocessor {
         case JString(s) => s
         case _ => "UNKNOWN"
       }
-
-      m + ((kind, m.getOrElse(kind, Vector()) :+ jv))
+      
+      m + ((kind, m.getOrElse(kind, Vector()) :+ xform(jv)))
     }
     
     (Map[String, Vector[JObject]]() /: jls)(partitionOne _)
   }
-
+  
   
 }
